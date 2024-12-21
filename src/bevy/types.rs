f9e87379aa4e019afa00377be4af9811e1d2130d
Liam Hamilton <liam.hamilton1999@hotmail.com>
2024-12-22 09:10:52 +1100

    Added IVec2 impl

--- src/bevy/types.rs
+++ src/bevy/types.rs
@@ -1,10 +1,19 @@
 use crate::{Arbitrary, Result, Unstructured};
 
-use bevy::prelude::Entity;
+use bevy::{math::IVec2, prelude::Entity};
 
-impl<'a> Arbitrary<'a> for bevy::prelude::Entity {
+impl<'a> Arbitrary<'a> for Entity {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
         let bits = u64::arbitrary(u)?;
+
         Ok(Entity::from_bits(bits))
+    }
+}
+
+impl<'a> Arbitrary<'a> for IVec2 {
+    fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
+        let x = i32::arbitrary(u)?;
+        let y = i32::arbitrary(u)?;
+        Ok(IVec2::new(x, y))
     }
 }
